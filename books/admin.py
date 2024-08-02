25e1fc49d43462a6fe5dd9316e3e922fe363eb1d
ElmurodAzodov <elmurodazodov002@gmail.com>
2024-08-02 13:45:12 +0500

    Introduction to Django Admin

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+from books.models import Book, Author, BookAuthor, BookReview
+
+class BookAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'isbn')
+    # list_filter = ('title',)
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+class BookAuthorAdmin(admin.ModelAdmin):
+    pass
+
+class BookReviewAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(BookAuthor, BookAuthorAdmin)
+admin.site.register(BookReview, BookReviewAdmin)
