00a73e3f2cb74290c9d1cba3e08590f1eff6bbcf
ElmurodAzodov <elmurodazodov002@gmail.com>
2024-08-03 17:56:46 +0500

    register users

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
 from django.views import View
 
 
@@ -7,6 +8,26 @@ from django.views import View
 class RegisterView(View):
     def get(self, request):
         return render(request, "users/register.html")
+    
+    def post(self, request):
+        username = request.POST['username']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        
+        user = User.objects.create(
+            username=username, 
+            first_name=first_name,
+            last_name=last_name,
+            email=email
+        )
+        user.set_password(password)
+        user.save()
+        
+        
+        
+        return redirect('users:login')
     
 class LoginView(View):
     def get(self, request):
